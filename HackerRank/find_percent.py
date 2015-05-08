39f4739e98992b31b344541292d8ae98e3d0559f
Hassan Abid <hassanabidpk89@gmail.com>
2015-05-09 00:46:53 +0900

    solved percent problem -not robust

--- HackerRank/find_percent.py
+++ HackerRank/find_percent.py
@@ -7,6 +7,7 @@ https://www.hackerrank.com/challenges/finding-the-percentage
 
 #number of students
 n = input()
+studentsDict = dict()
  
 # list of sujects
 subject = ['Name', 'Maths', 'Physics', 'Chemistry']
@@ -16,17 +17,29 @@ while n > 0:
     n -= 1
     data = raw_input().split(" ")
     name = data[0]
+    score = (data[1],data[2],data[3])
+    studentsDict[name] = score
     print name
-    for i in range(len(data)):
+    # for i in range(len(data)-1):
        
-        result = dict(zip(subject, data))
+        # result = dict(zip(subject, data))
+
+
    
-    name = result
-    print name
+    print "dict : ", studentsDict
+    # name = result
+    # print name
  
 # find a student's data
 findname = raw_input()
-if findname in name:
-    print "YES"
-else:
-    print "NO"
+total_score = 0
+for i in range(3):
+	total_score += float(studentsDict[findname][i])
+	print total_score
+
+average_score = total_score/3.0
+print "average Score : " + str(average_score)
+# if findname in name:
+#     print "YES"
+# else:
+#     print "NO"
