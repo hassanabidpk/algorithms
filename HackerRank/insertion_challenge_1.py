db12790b748b1f0b96a185876c30bf011afe9f9f
Hassan Abid <hassanabidpk89@gmail.com>
2015-05-11 00:21:37 +0900

    add initial code for hacker rank insertion sort 1

---
+++ HackerRank/insertion_challenge_1.py
@@ -0,0 +1,17 @@
+"""
+Given a sorted list with an unsorted number V in the rightmost cell, can 
+you write some simple code to insert V into the array so that it remains sorted?
+
+Print the array every time a value is shifted in the array until the array is fully sorted. 
+The goal of this challenge is to follow the correct order of insertion sort.
+"""
+
+# start implementation 
+
+def solution(input):
+
+	pass
+
+if __name__ == '__main__':
+	test_list1 = [2, 4, 6, 8, 3]
+	solution(test_list1)
