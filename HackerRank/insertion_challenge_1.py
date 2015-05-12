4e91910e1962b25a5291fec91c9b2514bacdb291
Hassan Abid <hassanabidpk89@gmail.com>
2015-05-12 21:22:40 +0900

    insertion_sort_challenge 1
    https://www.hackerrank.com/challenges/insertionsort1/submissions/code/11
    607609

--- HackerRank/insertion_challenge_1.py
+++ HackerRank/insertion_challenge_1.py
@@ -8,10 +8,28 @@ The goal of this challenge is to follow the correct order of insertion sort.
 
 # start implementation 
 
-def solution(input):
+#!/bin/python
+def insertionSort(ar):
+    check = len(ar) - 1
+    key = ar[len(ar) - 1]
+    while (ar[check] >= key and ar[check-1] >= key):
+    	ar[check] = ar[check -1]
+    	check -=1 
+    	print ' '.join(map(str, ar))
+    	if(check == 0):
+    		break
 
-	pass
+    ar[check] = key
+    print ' '.join(map(str, ar))
+    return ar
+
 
 if __name__ == '__main__':
 	test_list1 = [2, 4, 6, 8, 3]
-	solution(test_list1)
+	insertionSort(test_list1)
+
+# output format 
+# 2 4 6 8 8 
+# 2 4 6 6 8 
+# 2 4 4 6 8 
+# 2 3 4 6 8
